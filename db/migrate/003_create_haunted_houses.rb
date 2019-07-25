2ac645f0897df5e101af6d351d163d5d544f13f7
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-07-25 18:50:51 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,14 @@
-# Create your haunted_houses migration here
+class HauntedHouse < ActiveRecord::Migrate[4.2]
+  
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name 
+      t.string :location 
+      t.string :theme
+      t.integer :price
+      t.boolean :family_friendly
+      t.timestamps
+      t.text :description 
+    end
+  end
+end
